d071afd09c19e5e0972528c83b01a56d82c0b856
EnekoDev <enekolabdev@gmail.com>
2024-10-23 14:22:29 +0200

    first commit with code unfinished

---
+++ csv_to_json2.py
@@ -0,0 +1,30 @@
+import csv
+import json
+
+CSV_URL = '/home/olen/Documentos/workspaces/arratek-workspace/aplicaciones/afiliados.csv'
+JSON_URL = '/home/olen/Documentos/workspaces/arratek-workspace/aplicaciones/afiliados.json'
+
+JSON_HEADER = {
+    "version": 2,
+    "data": {
+        "api::centro-trabajo.centro-trabajo": {}
+    }
+}
+
+with open(CSV_URL, mode="r") as csvfile:
+    reader = csv.DictReader(csvfile)
+    
+    count = 0
+    
+    for row in reader:
+        count = count + 1
+        
+        id_key = str(count)
+        
+        JSON_HEADER["data"]["api::centro-trabajo.centro-trabajo"][id_key] = {
+            key: (int(value) if value.isdigit() else value)
+            for key, value in row.items()
+        }
+
+with open(JSON_URL, "w") as json_file:
+    json.dump(JSON_HEADER, json_file, indent=4)
