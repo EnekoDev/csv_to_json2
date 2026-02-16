e5b1818b2f2bf3e417a409e3524b16b944d4010d
EnekoDev <enekolabdev@gmail.com>
2026-02-16 11:08:06 +0100

    added .env and some comments

--- csv_to_json2.py
+++ csv_to_json2.py
@@ -1,27 +1,46 @@
+import os
 import csv
 import json
 
-JSON_HEADER = {
-    "version": 2,
-    "data": {
-        "api::centro-trabajo.centro-trabajo": {}
-    }
-}
+CSV_URL = os.environ['CSV_URL']
+JSON_URL = os.environ['JSON_URL']
+
+def str_to_bool(value):
+    if isinstance(value, str):
+        return value.lower() == 'true'
+    return value
+
+# As the process is quite slow I placed some messages to know that the script is running as I tend to overthink stuff too much
+print("BEGIN")
+
+all_users = []
 
 with open(CSV_URL, mode="r") as csvfile:
     reader = csv.DictReader(csvfile)
     
-    count = 0
+    count = 14  # The row from which you start adding the data, in this case from row 15 onwards
     
     for row in reader:
-        count = count + 1
-        
+        count += 1
         id_key = str(count)
+
+        user_data = {}
         
-        JSON_HEADER["data"]["api::centro-trabajo.centro-trabajo"][id_key] = {
-            key: (int(value) if value.isdigit() else value)
-            for key, value in row.items()
-        }
+        for key, value in row.items():
+            if value.isdigit():
+                user_data[key] = int(value)
+            elif value.lower() in ["true", "false"]:
+                user_data[key] = str_to_bool(value)
+            else:
+                user_data[key] = value
+        
+        all_users.append(user_data)
+
+JSON_HEADER = {"data": all_users}
+
+print("CONVERTED")
 
 with open(JSON_URL, "w") as json_file:
     json.dump(JSON_HEADER, json_file, indent=4)
+
+print("SAVED")
